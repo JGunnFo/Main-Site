3d4c7e21789013e699df5ec2b82edd334bdea32c
JGunnFo <jeffgunn814@gmail.com>
2020-06-17 15:33:42 -0700

    Added special corner borders for topSection js, and minor style changes.

--- src/subcomponents/topSection.js
+++ src/subcomponents/topSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import { connect } from "react-redux";
 import { GOTO, goTo} from "../Actions";
+import theL from './transparent.png';
 
 
 export function mainContentFrame(props){
@@ -15,9 +16,9 @@ export function mainContentFrame(props){
   function section_Main(props){
     return(
       <div className="Main-Grid-Parent Main-Flex-Parent">
-        <div className="Main-Text-Emph Line1">React Redux Front-End Developer.</div>
-        <div className="Main-Text-Deemph Line2">Jeffrey Gunn</div>
-        <div className="Main-Text-Emph Line3">Let's work together. [||]</div>
+        <div className="Main-Text-Emph Line1"><img className="InlineL" src={theL} />React Redux Front-End Developer.</div>
+        <div className="Main-Text-Deemph Line2"><img className="InlineL" src={theL} />Jeffrey Gunn</div>
+        <div className="Main-Text-Emph Line3"> <img className="InlineL" src={theL} /> Let's work together. [||]</div>
       </div>
     );
   }
