6dacb8d5b4c1587a95277cc5458634f3ffb1ece3
JGunnFo <jeffgunn814@gmail.com>
2020-07-28 11:16:31 -0700

    Adjusted text colors. Rewrote email button so that the svg could be adjusted based on positioning better. Updated snapshot. Reverted back to 4 navbar entries for nwo. Removed outline. Added a new query for largest sizes so that space is properly utilized when viewing about. Removed some extraneouous css. Made SVGs in navbar properly aligned with the text below them..

--- src/subcomponents/bottomSection.js
+++ src/subcomponents/bottomSection.js
@@ -9,13 +9,10 @@ export function navBar(props){
     return(
       <div className="Nav-Background">
       <div className="Nav-Grid-Parent Nav-Flex-Parent" role="navigation">
+      {switchForSection("About", props)}
+      {switchForSection("Main", props)}
       {switchForSection("Resume", props)}
-      {switchForSection("About", props)}
-      {switchForSection("Main", props)}
       {switchForSection("Github", props)}
-      {switchForSection("Main", props)}
-      {switchForSection("About", props)}
-      {switchForSection("Main", props)}
       </div>
       &nbsp;</div>
     );
@@ -35,7 +32,7 @@ function selectSVG(passTo){
 
 if (passTo==="Github"){
   return( 
-    <svg className="svg-icon svg-icon-marginGithub" aria-hidden="true" viewBox="0 0 1024 770.3333740234375">	
+    <svg className="svg-icon svg-icon-marginHigher" aria-hidden="true" viewBox="0 0 1024 770.3333740234375">	
     <path d="M236 184.667q-9-10-22.5-10t-22.5 10l-182 181q-9 9-9 22.5t9 22.5l182 182q9 10 22.5 10t22.5-10q10-9 10-22.5t-10-22.5l-159-159 159-159q10-10 10-23t-10-22zm552 0q9-10 22.5-10t22.5 10l182 181q9 9 9 22.5t-9 22.5l-182 182q-9 10-22.5 10t-22.5-10q-10-9-10-22.5t10-22.5l159-159-159-159q-10-10-10-23t10-22zm-97-180q12 6 16 19t-2 24l-371 704q-7 12-19.5 16t-24.5-2q-11-7-15-19.5t2-24.5l371-703q6-12 18.5-16t24.5 2z">
     </path>
 						</svg>
@@ -57,7 +54,7 @@ if (passTo==="Main"){
 
 if (passTo==="Resume"){
   return( 
-<svg className="svg-icon svg-icon-marginResume" aria-hidden="true" viewBox="0 0 20 20">
+<svg className="svg-icon svg-icon-marginHigher" aria-hidden="true" viewBox="0 0 20 20">
 							<path fill="none" d="M2.25,12.584c-0.713,0-1.292,0.578-1.292,1.291s0.579,1.291,1.292,1.291c0.713,0,1.292-0.578,1.292-1.291S2.963,12.584,2.25,12.584z M2.25,14.307c-0.238,0-0.43-0.193-0.43-0.432s0.192-0.432,0.43-0.432c0.238,0,0.431,0.193,0.431,0.432S2.488,14.307,2.25,14.307z M5.694,6.555H18.61c0.237,0,0.431-0.191,0.431-0.43s-0.193-0.431-0.431-0.431H5.694c-0.238,0-0.43,0.192-0.43,0.431S5.457,6.555,5.694,6.555z M2.25,8.708c-0.713,0-1.292,0.578-1.292,1.291c0,0.715,0.579,1.292,1.292,1.292c0.713,0,1.292-0.577,1.292-1.292C3.542,9.287,2.963,8.708,2.25,8.708z M2.25,10.43c-0.238,0-0.43-0.192-0.43-0.431c0-0.237,0.192-0.43,0.43-0.43c0.238,0,0.431,0.192,0.431,0.43C2.681,10.238,2.488,10.43,2.25,10.43z M18.61,9.57H5.694c-0.238,0-0.43,0.192-0.43,0.43c0,0.238,0.192,0.431,0.43,0.431H18.61c0.237,0,0.431-0.192,0.431-0.431C19.041,9.762,18.848,9.57,18.61,9.57z M18.61,13.443H5.694c-0.238,0-0.43,0.193-0.43,0.432s0.192,0.432,0.43,0.432H18.61c0.237,0,0.431-0.193,0.431-0.432S18.848,13.443,18.61,13.443z M2.25,4.833c-0.713,0-1.292,0.578-1.292,1.292c0,0.713,0.579,1.291,1.292,1.291c0.713,0,1.292-0.578,1.292-1.291C3.542,5.412,2.963,4.833,2.25,4.833z M2.25,6.555c-0.238,0-0.43-0.191-0.43-0.43s0.192-0.431,0.43-0.431c0.238,0,0.431,0.192,0.431,0.431S2.488,6.555,2.25,6.555z"></path>
 						</svg>
 
